da09f583ff19a0e63ab6b9591a4e3920fd2eb889
Anfe-ion <andres.moncada.c@gmail.com>
2022-06-04 19:07:46 -0500

    Messi

---
+++ logic.js
@@ -0,0 +1,37 @@
+const getRemainingTime = deadline => {
+    let now = new Date(),
+        remainTime = (new Date(deadline) - now + 1000) / 1000,
+        remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
+        remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
+        remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
+        remainDays = Math.floor(remainTime / (3600 * 24));
+  
+    return {
+      remainSeconds,
+      remainMinutes,
+      remainHours,
+      remainDays,
+      remainTime
+    }
+  };
+  
+  const countdown = (deadline,elem,finalMessage) => {
+    const el = document.getElementById(elem);
+  
+    const timerUpdate = setInterval( () => {
+      let t = getRemainingTime(deadline);
+      el.innerHTML = `Faltan ${t.remainDays} días 
+      ${t.remainHours} horas ${t.remainMinutes} minutos y 
+      ${t.remainSeconds} segundos <br> para que el mejor jugador de la
+      historia termine contrato`;
+  
+      if(t.remainTime <= 1) {
+        clearInterval(timerUpdate);
+        el.innerHTML = finalMessage;
+      }
+  
+    }, 1000)
+  };
+  
+  countdown('Jun 30 2023 23:59:59 GMT+0200', 'clock', 
+  'Messi termino contrato con el PeCheGe');
